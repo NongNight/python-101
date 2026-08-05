e2050b306464257af1e1d10163c6dad4e85f497d
NongNight <JansanitR@gmail.con>
2026-08-05 08:09:53 +0700

    Lists and Tuples

---
+++ LECTURE06/main.py
@@ -0,0 +1,8 @@
+# prime_number = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+# print(f"Prime number: {prime_number}")
+# fifth_prime = prime_number[4]
+# print(f"Fifth prime number: {fifth_prime}")
+
+colors = ["red", "blue", "green", "yellow", "purple"]
+second_to_last_color = colors[-2]
+print(f"Second to last color: {second_to_last_color}")
